ebfb6692f0842df9d6e047c80d1b9d23146ce15f
Kabal767 <nifacucu@gmail.com>
2026-09-25 12:01:59 -0300

    Removed the discarded dicesets

--- scripts/module.js
+++ scripts/module.js
@@ -1,23 +1,7 @@
 Hooks.on("diceSoNiceReady", (dice3d) => {
-    dice3d.addSystem({ id: "voyeur", name: "(+)Voyeur", group: "Voyeur Conejo" }, "default");
-    dice3d.addSystem({ id: "artDeco", name: "ArtDeco", group: "Voyeur Conejo" }, "default");
     dice3d.addSystem({ id: "carrion", name: "Carrion", group: "Voyeur Conejo" }, "default");
     dice3d.addSystem({ id: "urik", name: "Urik", group: "Voyeur Conejo" }, "default");
     dice3d.addSystem({ id: "urikV2", name: "UrikV2", group: "Voyeur Conejo" }, "default");
-
-    //VOYEUR
-    dice3d.addDicePreset({                
-        type: 'd6',
-        modelFile: 'modules/vc-customdices/models/voyeur/d6-voyeur.glb',
-        system: 'voyeur'
-    });
-
-    //ART-DECO
-    dice3d.addDicePreset({                
-        type: 'd6',
-        modelFile: 'modules/vc-customdices/models/artDeco/d6-artDeco.glb',
-        system: 'artDeco'
-    });
 
     //CARRION
     dice3d.addDicePreset({                
